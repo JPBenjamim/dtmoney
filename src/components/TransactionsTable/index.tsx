c111d1dd505b047e163dd16794018f7381838629
Jpbenjamim <dev.bejamim@gmail.com>
2022-03-26 06:09:40 -0300

    initial commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+    useEffect(() => {
+        fetch('http://localhost:3000/api/transactions')
+        .then(response => response.json())
+        .then(data =>console.log(data))
+    },[]);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>website</td>
+            <td className="deposit">R$10000</td>
+            <td>Deselvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw"> -R$1200</td>
+            <td>Casa</td>
+            <td>25/02/2022</td>
+          </tr>
+
+         
+        </tbody>
+      </table>
+    </Container>
+  );
+}
